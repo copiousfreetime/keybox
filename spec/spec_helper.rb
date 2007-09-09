78a6abf794341f0b12f0e24f000f816d05017376
Jeremy Hinegardner <jeremy@hinegardner.org>
2007-09-09 04:42:56 +0000

    raise error if termios is found during testing
    20070909044256-2f820-12a2fe880bb24639ec7de01c19aa4820b5dbec7f.gz

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,15 @@ begin
     require 'spec'
     require 'stringio'
     require 'tempfile'
+    
+    # termios cannot be used during testing
+    begin
+        require 'termios'
+        raise "ERROR!!!! termios library found.  It is not possible to run keybox tests with the termios library at this time."
+        exit 2
+    rescue LoadError
+    end
+    
 rescue LoadError
     path = File.expand_path(File.join(File.dirname(__FILE__),"..","lib"))
     raise if $:.include? path
